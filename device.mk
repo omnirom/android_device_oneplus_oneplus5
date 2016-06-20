def6727760bad2b15165fad3fa136b5bc41c1405
Kumar, Anand <anandkumar@codeaurora.org>
2016-06-20 19:03:06 +0530

    msm8996: Delete firmware file for msm8996
    Delete the dat and nv bin file for 8996,as these are not
    used for rome platform.
    
    CRs-Fixed: 1014722
    Change-Id: Ic8ba562da76bf772c86d0f9a04b1ea8098b0c552

--- device.mk
+++ device.mk
@@ -216,7 +216,6 @@ PRODUCT_PACKAGES += \
 # Wifi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
-    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/qca_cld/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
