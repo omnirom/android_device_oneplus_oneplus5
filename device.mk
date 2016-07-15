2d103e28818f6eee75b71d206e31c729f43e1322
maxwen <max.weninger@gmail.com>
2016-07-15 22:52:46 +0200

    overlay chages
    https://github.com/CyanogenMod/android_device_oneplus_oneplus3/commit/52e5ad79f18b7f8402c09593f9b034b7afb5a2c5
    https://github.com/CyanogenMod/android_device_oneplus_oneplus3/commit/ee5d6bbb7159742843378a72bb68a96a19d88eb3
    
    enable ipa stuff

--- device.mk
+++ device.mk
@@ -205,18 +205,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/qca_cld/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/wifi/hostapd.accept:system/etc/hostapd/hostapd.accept \
-    $(LOCAL_PATH)/wifi/hostapd.conf:system/etc/hostapd/hostapd_default.conf \
-    $(LOCAL_PATH)/wifi/hostapd.deny:system/etc/hostapd/hostapd.deny \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
-#PRODUCT_PACKAGES += \
-#    ipacm \
-#    ipacm-diag \
-#    IPACM_cfg.xml \
-#    libqsap_sdk \
-#    libQWiFiSoftApCfg
+PRODUCT_PACKAGES += \
+    ipacm \
+    ipacm-diag \
+    IPACM_cfg.xml
 
 PRODUCT_PACKAGES += \
     libwpa_client \
