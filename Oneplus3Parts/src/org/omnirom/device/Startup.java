omnirom/android_device_oneplus_oneplus3

[2/2] Oneplus3Parts: add support for gesture launch activities
launch activities from device key handlers
add app select preference

Change-Id: I876e679c76bd2b7247cbcd18c9adea006d399bcf

Oneplus3Parts/src/org/omnirom/device/Startup.java
@@ -20,17 +20,46 @@ package org.omnirom.device;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class Startup extends BroadcastReceiver {
 
+    public static void restore(Context context, String file, boolean enabled) {
+        if (file == null) {
+            return;
+        }
+        Utils.writeValue(file, enabled ? "1" : "0");
+    }
+
+    public static void restore(Context context, String file, String value) {
+        if (file == null) {
+            return;
+        }
+        Utils.writeValue(file, value);
+    }
+
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
-        CameraGestureSwitch.restore(context);
-        TorchGestureSwitch.restore(context);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_TORCH_SWITCH, false);
+        restore(context, TorchGestureSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_UP_ARROW_SWITCH, false);
+        restore(context, UpArrowGestureSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_CAMERA_SWITCH, false);
+        restore(context, CameraGestureSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_MUSIC_SWITCH, false);
+        restore(context, MusicGestureSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
+        restore(context, SRGBModeSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_HBM_SWITCH, false);
+        restore(context, HBMModeSwitch.getFile(), enabled ? "2" : "0");
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_LEFT_ARROW_SWITCH, false);
+        restore(context, LeftArrowGestureSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_RIGHT_ARROW_SWITCH, false);
+        restore(context, RightArrowGestureSwitch.getFile(), enabled);
+
         KeyHandler.setButtonDisable(context);
         VibratorStrengthPreference.restore(context);
-        MusicGestureSwitch.restore(context);
-        SRGBModeSwitch.restore(context);
-        HBMModeSwitch.restore(context);
     }
 }

omnirom/android_device_oneplus_oneplus5

Oneplus3Parts/src/org/omnirom/device/Startup.java
@@ -20,17 +20,46 @@ package org.omnirom.device;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class Startup extends BroadcastReceiver {
 
+    public static void restore(Context context, String file, boolean enabled) {
+        if (file == null) {
+            return;
+        }
+        Utils.writeValue(file, enabled ? "1" : "0");
+    }
+
+    public static void restore(Context context, String file, String value) {
+        if (file == null) {
+            return;
+        }
+        Utils.writeValue(file, value);
+    }
+
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
-        CameraGestureSwitch.restore(context);
-        TorchGestureSwitch.restore(context);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean enabled = sharedPrefs.getBoolean(GestureSettings.KEY_TORCH_SWITCH, false);
+        restore(context, TorchGestureSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(GestureSettings.KEY_UP_ARROW_SWITCH, false);
+        restore(context, UpArrowGestureSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(GestureSettings.KEY_CAMERA_SWITCH, false);
+        restore(context, CameraGestureSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(GestureSettings.KEY_MUSIC_SWITCH, false);
+        restore(context, MusicGestureSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
+        restore(context, SRGBModeSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_HBM_SWITCH, false);
+        restore(context, HBMModeSwitch.getFile(), enabled ? "2" : "0");
+        enabled = sharedPrefs.getBoolean(GestureSettings.KEY_LEFT_ARROW_SWITCH, false);
+        restore(context, LeftArrowGestureSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(GestureSettings.KEY_RIGHT_ARROW_SWITCH, false);
+        restore(context, RightArrowGestureSwitch.getFile(), enabled);
+
         KeyHandler.setButtonDisable(context);
         VibratorStrengthPreference.restore(context);
-        MusicGestureSwitch.restore(context);
-        SRGBModeSwitch.restore(context);
-        HBMModeSwitch.restore(context);
     }
 }