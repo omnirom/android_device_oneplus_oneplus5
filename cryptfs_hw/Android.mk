b17eacbd7b923973ff4953ab05a19939a06553f7
Marko Man <darkobas@gmail.com>
2017-07-22 22:14:23 +0200

    import cryptfs_hw from caf LA.UM.5.7.r1-10200-8x98.0

--- cryptfs_hw/Android.mk
+++ cryptfs_hw/Android.mk
@@ -24,10 +24,6 @@ LOCAL_SHARED_LIBRARIES := $(commonSharedLibraries)
 
 LOCAL_MODULE_OWNER := qcom
 
-ifeq ($(TARGET_SWV8_DISK_ENCRYPTION),true)
-LOCAL_CFLAGS += -DCONFIG_SWV8_DISK_ENCRYPTION
-endif
-
 # USE_ICE_FOR_STORAGE_ENCRYPTION would be true in future if
 # TARGET_USE_EMMC_USE_ICE is set
 ifeq ($(TARGET_USE_UFS_ICE),true)
