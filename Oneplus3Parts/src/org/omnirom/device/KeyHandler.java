01acc920df3eeb35baf21ee9428aafea19323b34
kldoc <fred.berbigier@gmail.com>
2017-07-28 08:29:13 +0800

    Fix gesture control path and FPC control path
    Change-Id: I0e6773c7bdcd6f557956df0667d3b9bc809984e0

--- Oneplus3Parts/src/org/omnirom/device/KeyHandler.java
+++ Oneplus3Parts/src/org/omnirom/device/KeyHandler.java
@@ -59,8 +59,8 @@ public class KeyHandler implements DeviceKeyHandler {
     private static final boolean DEBUG = true;
     protected static final int GESTURE_REQUEST = 1;
     private static final int GESTURE_WAKELOCK_DURATION = 2000;
-    private static final String KEY_CONTROL_PATH = "/proc/s1302/virtual_key";
-    private static final String FPC_CONTROL_PATH = "/sys/devices/soc/soc:fpc_fpc1020/proximity_state";
+    private static final String KEY_CONTROL_PATH = "/proc/touchpanel/key_disable";
+    private static final String FPC_CONTROL_PATH = "/sys/module/fpc1020_tee/parameters/ignor_home_for_ESD";
 
     private static final int GESTURE_CIRCLE_SCANCODE = 250;
     private static final int GESTURE_V_SCANCODE = 252;
