b3bad15bbd799d028927dc62e0dfa946b394968f
Marko Man <darkobas@gmail.com>
2017-08-07 14:22:42 +0200

    op5 prop: add a few missing props
    Change-Id: I1589944e9c6a3b41776f5c2970722cea0fbe2dbb

--- device.mk
+++ device.mk
@@ -22,7 +22,6 @@
 
 $(call inherit-product, vendor/oneplus/oneplus5/device-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-4096-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
