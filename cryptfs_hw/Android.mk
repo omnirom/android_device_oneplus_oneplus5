470c3d5e8e65e4a541d086a224f024c4838443c1
Marko Man <darkobas@gmail.com>
2017-09-04 13:31:47 +0200

    op5: cryptfs update from CAF
    Change-Id: I2fb3be22e3f0a044499d562797557bd4f22d2d74

--- cryptfs_hw/Android.mk
+++ cryptfs_hw/Android.mk
@@ -2,8 +2,6 @@ ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_PROPRIETARY_MODULE := true
-
 sourceFiles := \
                cryptfs_hw.c
 
@@ -11,9 +9,14 @@ commonSharedLibraries := \
                         libcutils \
                         libutils \
                         libdl \
-                        libhardware
+                        libhardware \
+                        liblog
+
 commonIncludes := \
-                  hardware/libhardware/include/hardware/
+                  hardware/libhardware/include/hardware/ \
+                  $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
+
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_C_INCLUDES := $(commonIncludes)
 LOCAL_SRC_FILES := $(sourceFiles)
