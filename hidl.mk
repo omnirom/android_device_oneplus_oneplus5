f06455d1ed53719f54e2754d398943917b6e98d9
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-09-04 09:04:45 +0200

    op5: add netutils-wrapper impl
    Change-Id: I80562b014c3457f813f8e1313a2f29c93bbc674c

--- hidl.mk
+++ hidl.mk
@@ -44,6 +44,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl
 
+# Netutils
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl
