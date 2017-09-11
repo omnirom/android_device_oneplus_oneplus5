a843598c6e8b7fc493a3f02ba76cdfc1acb31b6f
Marko Man <darkobas@gmail.com>
2017-09-11 11:46:49 +0200

    op5: enable hidl power service
    Change-Id: Ia9c2222664343200f1e0c99b97f0fd7a7a235c8c

--- hidl.mk
+++ hidl.mk
@@ -68,7 +68,9 @@ PRODUCT_PACKAGES += \
  
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
+
 
 
 # RenderScript HAL
