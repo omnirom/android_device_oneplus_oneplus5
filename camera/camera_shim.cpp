af31b732a91167d68973cac1a40b8949029e3bdd
Marko Man <darkobas@gmail.com>
2017-09-26 11:30:44 +0200

    op5: cleanup camera shims
    Change-Id: Iee9b5fbaabd625aeded3f110fc5c538db213d40e

--- camera/camera_shim.cpp
+++ camera/camera_shim.cpp
@@ -14,4 +14,48 @@
  * limitations under the License.
  */
 
+#include <stdint.h>
+
 const char *_ZN7android18gClientPackageNameE;
+
+
+// int32_t qcamera::QCameraParameters::setQuadraCfaMode(uint32_t enable, bool initCommit)
+// _ZN7qcamera17QCameraParameters16setQuadraCfaModeEjb
+int32_t _ZN7qcamera17QCameraParameters16setQuadraCfaModSHIM(uint32_t, bool)
+{
+    return 0;
+}
+
+// int32_t qcamera::QCameraParameters::setQuadraCfa(const QCameraParameters& params)
+// _ZN7qcamera17QCameraParameters12setQuadraCfaERKS0_
+int32_t _ZN7qcamera17QCameraParameters12setQuadraCfaERSHIM(const void *)
+{
+    return 0;
+}
+
+// bool qcamera::QCameraParameters::getQuadraCfa()
+// _ZN7qcamera17QCameraParameters12getQuadraCfaEv
+bool _ZN7qcamera17QCameraParameters12getQuadraCSHIM(void)
+{
+    return false;
+}
+
+// bool qcamera::isOneplusCamera(qcamera *this)
+// _ZN7qcamera15isOneplusCameraEv
+bool _ZN7qcamera15isOneplusCameSHIM(const void *)
+{
+    return true;
+}
+
+// bool qcamera::QCameraParameters::is3p8spLowLight(qcamera::QCameraParameters *this)
+// _ZN7qcamera17QCameraParameters15is3p8spLowLightEv
+bool _ZN7qcamera17QCameraParameters15is3p8spLowLigSHIM(const void *)
+{
+    return true;
+}
+
+extern "C" void _ZN7android19IGraphicBufferAlloc10descriptorE() {}
+extern "C" void _ZN7android13GraphicBuffer10reallocateEjjij() {}
+extern "C" void _ZN7android13GraphicBufferC1EjjijjP13native_handleb() {}
+extern "C" void _ZN7android20DisplayEventReceiverC1Ev() {}
+
