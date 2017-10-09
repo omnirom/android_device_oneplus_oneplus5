89ed5c40da812eb332eccd79ebc47e57b139c388
Marko Man <darkobas@gmail.com>
2017-10-09 21:44:13 +0200

    sepolololol
    Change-Id: Id397b4689ff143967dbe7fdc9f47c522193c06e2
    
    speololll
    
    Change-Id: Ic5d9c61a907939c7639fce319426a0c3515fe1a7
    
    sepololol
    
    Change-Id: I2bec354b4422272c5915de07bc4a83a0b71d38fa

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -256,8 +256,7 @@ BOARD_USES_QCNE := true
 BOARD_USES_QC_TIME_SERVICES := true
 
 # selinux
-include device/qcom/sepolicy/sepolicy.mk
-
+#include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(BOARD_PATH)/sepolicy
 
 BOARD_SECCOMP_POLICY += $(BOARD_PATH)/seccomp
