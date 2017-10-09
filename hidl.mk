d24cb1cc84a7f9da1059651b8071acf1b6a7569a
Marko Man <darkobas@gmail.com>
2017-10-09 21:43:41 +0200

    op5: many small things
    Change-Id: Ib7ef9e917644b07d35a7e8afa4e3005ac9667911

--- hidl.mk
+++ hidl.mk
@@ -2,8 +2,7 @@
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.audio.effect@2.0-impl
 
 # Bluetooth
 PRODUCT_PACKAGES += \
@@ -29,8 +28,7 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service \
-    android.hardware.configstore@1.0-service \
-    android.hardware.broadcastradio@1.0-impl
+    android.hardware.configstore@1.0-service
 
 PRODUCT_PACKAGES += \
     vendor.display.color@1.0-service \
