24b903e1d90d00d9bd480e7f7c71d36a30069c08
Marko Man <darkobas@gmail.com>
2017-10-18 09:12:37 +0200

    op5: add soundtrigger impl hal
    Change-Id: Ib0915b4890a53b0619d0ffac8ba63fc241c24854

--- hidl.mk
+++ hidl.mk
@@ -2,7 +2,8 @@
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.soundtrigger@2.0-impl
 
 # Bluetooth
 PRODUCT_PACKAGES += \
