220c1bcbabf4f738ddd396b5875dbe0cc7d7f5e6
Marko Man <darkobas@gmail.com>
2017-10-18 18:58:58 +0200

    op5: add setFeature api to power hal
    Change-Id: I1d058880b16fa29d377296573b51c0f08d67a050

--- power/Android.mk
+++ power/Android.mk
@@ -22,7 +22,7 @@ endif
 
 LOCAL_MODULE := power.msm8998
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS := -Wno-unused-parameter -Wno-unused-variable
+LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
