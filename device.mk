e5fea30bff6e5f33c801523b793815d4aabefa83
Marko Man <darkobas@gmail.com>
2017-11-22 14:00:23 +0100

    op5: inherit CarrierConfig overlay
    Change-Id: Id44e0a7ff1e79f0ebe7dd64928f8bb7d7bf716d6

--- device.mk
+++ device.mk
@@ -303,6 +303,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+PRODUCT_PACKAGE_OVERLAYS += vendor/omni/overlay/CarrierConfig
+
 # Temporary handling
 #
 # Include config.fs get only if legacy device/qcom/<target>/android_filesystem_config.h
