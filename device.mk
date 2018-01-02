19d15488cc1f1c21d08190f6d63e78d73006e80d
Marko Man <darkobas@gmail.com>
2018-01-03 00:18:08 +0100

    op5: build libnl_2
    Change-Id: I9b491029e7ae350da28b3188ac168826ba80949d

--- device.mk
+++ device.mk
@@ -307,6 +307,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     DeviceParts
 
+LIB_NL := libnl_2
+PRODUCT_PACKAGES += $(LIB_NL)
+
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
