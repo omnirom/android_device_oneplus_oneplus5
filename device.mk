9d9d3d6aea884c0f7723af52ae9c4e16e1cd35f0
Marko Man <darkobas@gmail.com>
2018-01-10 02:09:35 +0100

    op5: add bt and ant blobs
    fm blobs are unfortunately linked
    
    Change-Id: I6fa6849025a265b58e64310d428f088bcb4fe139

--- device.mk
+++ device.mk
@@ -253,13 +253,9 @@ PRODUCT_PACKAGES += \
     powerhint.qti
 
 # ANT+
-#PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library \
     libantradio
-
-#PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
