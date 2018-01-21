dde0b50acf343c6b4cdf78ed0c39d44d074f0a2b
maxwen <max.weninger@gmail.com>
2018-01-21 02:19:33 +0100

    op5: fix FC on gestures settings on remove fpc gestures prefs
    Change-Id: I2a954409a9c0f7226b43456da6d804ecb5746f7c

--- DeviceParts/src/org/omnirom/device/GestureSettings.java
+++ DeviceParts/src/org/omnirom/device/GestureSettings.java
@@ -190,10 +190,8 @@ public class GestureSettings extends PreferenceFragment implements
             mFPLeftSwipeApp.setValue(value);
             mFPLeftSwipeApp.setOnPreferenceChangeListener(this);
         } else {
-            PreferenceCategory fpGestures =
-                  (PreferenceCategory) findPreference(KEY_FP_GESTURE_CATEGORY);
-            PreferenceCategory fpGesturesDefault =
-                  (PreferenceCategory) findPreference(KEY_FP_GESTURE_DEFAULT_CATEGORY);
+            Preference fpGestures = findPreference(KEY_FP_GESTURE_CATEGORY);
+            Preference fpGesturesDefault = findPreference(KEY_FP_GESTURE_DEFAULT_CATEGORY);
             getPreferenceScreen().removePreference(fpGestures);
             getPreferenceScreen().removePreference(fpGesturesDefault);
         }
@@ -332,7 +330,11 @@ public class GestureSettings extends PreferenceFragment implements
     @Override
     public void onResume() {
         super.onResume();
+        if (mFPDownSwipeApp != null) {
             mFPDownSwipeApp.setEnabled(!areSystemNavigationKeysEnabled());
+        }
+        if (mFPUpSwipeApp != null) {
             mFPUpSwipeApp.setEnabled(!areSystemNavigationKeysEnabled());
+        }
     }
 }
