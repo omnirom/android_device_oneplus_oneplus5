b5d7f5f9e33d85fbded58bbc4fb872319e5c18e3
kldoc <fred.berbigier@gmail.com>
2018-01-31 20:37:00 +0800

    Oneplus5t: Add scrolling page up/down gesture
    Add scroll Page Up/Down for browsers
    
    Change-Id: I28798ad845e86f3802d1f9c2a70f78417df620eb

--- DeviceParts/src/org/omnirom/device/AppSelectListPreference.java
+++ DeviceParts/src/org/omnirom/device/AppSelectListPreference.java
@@ -61,6 +61,8 @@ public class AppSelectListPreference extends CustomDialogPreference {
     public static final String WAKE_ENTRY = "wake";
     public static final String VOLUME_UP_ENTRY = "volume_up";
     public static final String VOLUME_DOWN_ENTRY = "volume_down";
+    public static final String BROWSE_SCROLL_DOWN_ENTRY = "browse_scroll_down";
+    public static final String BROWSE_SCROLL_UP_ENTRY = "browse_scroll_up";
 
     private AppSelectListAdapter mAdapter;
     private Drawable mAppIconDrawable;
@@ -147,6 +149,14 @@ public class AppSelectListPreference extends CustomDialogPreference {
                 PackageItem volumeDownItem = new PackageItem(getContext().getResources().getString(R.string.volume_down),
                         R.drawable.ic_settings_sound, VOLUME_DOWN_ENTRY);
                 mInstalledPackages.add(0, volumeDownItem);
+
+                PackageItem browseScrollDownItem = new PackageItem(getContext().getResources().getString(R.string.browse_scroll_down),
+                        R.drawable.arrow_collapse_down, BROWSE_SCROLL_DOWN_ENTRY);
+                mInstalledPackages.add(0, browseScrollDownItem);
+
+                PackageItem browseScrollUpItem = new PackageItem(getContext().getResources().getString(R.string.browse_scroll_up),
+                        R.drawable.arrow_collapse_up, BROWSE_SCROLL_UP_ENTRY);
+                mInstalledPackages.add(0, browseScrollUpItem);
 
                 PackageItem disabledItem = new PackageItem(getContext().getResources().getString(R.string.disabled_entry),
                         R.drawable.ic_disabled, DISABLED_ENTRY);
@@ -348,6 +358,12 @@ public class AppSelectListPreference extends CustomDialogPreference {
                 } else if (name.equals(VOLUME_DOWN_ENTRY)) {
                     mTitle = getContext().getResources().getString(R.string.volume_down);
                     mAppIconResourceId = R.drawable.ic_settings_sound;
+                } else if (name.equals(BROWSE_SCROLL_DOWN_ENTRY)) {
+                    mTitle = getContext().getResources().getString(R.string.browse_scroll_down);
+                    mAppIconResourceId = R.drawable.arrow_collapse_down;
+                } else if (name.equals(BROWSE_SCROLL_UP_ENTRY)) {
+                    mTitle = getContext().getResources().getString(R.string.browse_scroll_up);
+                    mAppIconResourceId = R.drawable.arrow_collapse_up;
                 } else {
                     ComponentName componentName = ComponentName.unflattenFromString(name);
                     PackageItem item = mAdapter.resolveApplication(componentName);
