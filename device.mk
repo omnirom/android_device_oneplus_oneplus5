3f40398a6cae180f156e14ddf3b6fa7a56660eb6
Marko Man <darkobas@gmail.com>
2018-02-01 08:15:06 +0100

    op5/t: build vndk libs
    Change-Id: Iea92132853eefffa0996896f7786e5b331e83236

--- device.mk
+++ device.mk
@@ -305,6 +305,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     DeviceParts
 
+PRODUCT_PACKAGES += \
+    vndk-sp
+
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
